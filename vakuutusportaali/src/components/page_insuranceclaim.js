4d9796648a98e4c06ff2a1a91f437f430646f639
Kalle Pusa <kalle.pusa@gmail.com>
2018-08-30 12:03:12 +0300

    Added findall customer route. Added the beginnings of the insurance claim system /KPU

---
+++ vakuutusportaali/src/components/page_insuranceclaim.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { Grid, Row, Col } from 'react-bootstrap';
+import {connect} from 'react-redux';
+import ClaimForm from './form_insuranceclaim';
+import {addPost} from '../actions/postActions';
+
+export default class page_insuranceclaim extends Component {
+ 
+  render() {
+    return (
+      <div>
+            <Grid fluid className="splash"> 
+                    <Row className="show-grid cards text-center">
+                        < ClaimForm />
+                    </Row>
+                </Grid>
+      </div>
+    )
+  }
+}
