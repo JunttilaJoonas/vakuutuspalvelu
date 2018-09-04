18e887d02f919c30f94b4c972bdda179d7c6cafe
joonvena@gmail.com <joonvena@gmail.com>
2018-09-04 10:19:36 +0300

    Refactors & comments to code

--- adminclient/src/reducers/reducer_users.js
+++ adminclient/src/reducers/reducer_users.js
@@ -1,24 +1,36 @@
 import { GET_USERS, POST_USER, GET_USER_BY_ID, DELETE_USER_BY_ID, ADD_PROFILE_TO_USER, UPDATE_USER_PROFILE } from '../actions/types';
 
 export default function users(state = [], action)  {
+
+    /**
+    * @desc Action types for user handling
+    */
+
     switch(action.type) {
+
+        // Get all users
         case GET_USERS:
         return action.users;
         default:
         return state;
 
+        // Get one user by id
         case GET_USER_BY_ID:
         return action.users;
-
+        
+        // Add new user
         case POST_USER:
         return action.users;
         
+        // Add profile to new user
         case ADD_PROFILE_TO_USER:
         return action.users;
 
+        // Delete user 
         case DELETE_USER_BY_ID:
         return action.users;
 
+        // Update user information
         case UPDATE_USER_PROFILE:
         return action.users;
 
