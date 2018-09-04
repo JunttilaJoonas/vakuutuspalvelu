9614890c09f4ebfbc7bf9b27653b5beefe9ef32c
JunttilaJoonas <junttila.joonas@gmail.com>
2018-09-04 15:38:29 +0300

    uusi taustakuva?

--- vakuutusportaali/src/components/page_index.js
+++ vakuutusportaali/src/components/page_index.js
@@ -9,17 +9,9 @@ class IndexPage extends Component {
                 <Grid fluid className="splash">
                     <Row className="show-grid text-center information">
                         <Col xs={12} sm={12}>
-                            <h3>
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                                when an unknown printer took a galley of type and scrambled it to make a type specimen
-                                book.
-                                It has survived not only five centuries, but also the leap into electronic typesetting,
-                                remaining essentially unchanged. It was popularised in the 1960s with the release of
-                                Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-                                publishing
-                                software like Aldus PageMaker including versions of Lorem Ipsum.
-                            </h3>
+                            <h1>
+                                Vakuutusportaali on uudenlainen tapa käsitellä vakuutusasioitasi.
+                            </h1>
                             <a href="/laskevakuutus"><Button bsClass="insurance_button">Osta vakuutuksia</Button></a>
                         </Col>
                     </Row>
